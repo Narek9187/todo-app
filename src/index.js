cf21db9d2c63663722d3241dc5e395ba3ffbce83
9187narek* <narek9187@gmail.com>
2023-03-01 23:46:44 +0400

    switch bootstrap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const AppHeader = () => {
+  return <h1 className={"text-center text-light"}>ToDo List</h1>
+}
+
+const Search = () => {
+  return (
+      <div className={"w-50 mx-auto my-4 input-group"}>
+          <input className={"form-control"} placeholder='Search...'/>
+          <button className={"btn btn-outline-secondary bg-danger text-light border"}>Button</button>
+      </div>
+  )
+}
+
+const List = () => {
+  return (
+    <ul className={"list-unstyled text-center h4 text-light mt-5"}>
+      <li className={"justify-content-between d-flex mx-auto w-50 my-3"}>
+          <div>Learn react</div>
+          <button className={"btn btn-outline-secondary bg-warning text-light"}>delete</button>
+      </li>
+      <li className={"justify-content-between d-flex mx-auto w-50 my-3"}>
+          <div>Learn redux</div>
+          <button className={"btn btn-outline-secondary bg-warning text-light"}>delete</button>
+      </li>
+        <li className={"justify-content-between d-flex mx-auto w-50 my-3"}>
+          <div>Create app</div>
+          <button className={"btn btn-outline-secondary bg-warning text-light"}>delete</button>
+      </li>
+    </ul>
+  )
+}
+
+const App = () => {
+  return (
+    <div className={"container mt-5 pt-5 bg-primary border rounded-4"}>
+      <AppHeader/>
+      <Search/>
+      <List/>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App/>);
